dd34d992f2b556a719cf219f28649a822f9928e2
Masumsv <165934636+Masumsv@users.noreply.github.com>
2025-04-14 20:29:11 +0330

    Add files via upload

---
+++ oop3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+class parent {
+protected
+      virtual T Add(T a, T b) {
+        return a + b;
+    }
+};
+classs child:parent<int> {
+    public:
+     int Add(int a, int b) override {
+           int c = parent<int>::Add(5, 10);
+           return c + 20;
+    }
+};
+int main() {
+    child obj;
+    int result = obj.Add(3, 4);
+    cout << "Result: " << result << endl;
+    return 0;
